a64986b44f9d649e4a96347c775043f637b95df8
Kevin Malekera <r0900654@student.thomasmore.be>
2024-11-21 18:16:10 +0100

    feat: added renderer options and a plane geometry

--- main.js
+++ main.js
@@ -3,4 +3,56 @@
 // Import stylesheet
 import "./style.css";
 
-import * as THREE from 'three'; const scene = new THREE.Scene(); const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); const renderer = new THREE.WebGLRenderer(); renderer.setSize( window.innerWidth, window.innerHeight ); renderer.setAnimationLoop( animate ); document.body.appendChild( renderer.domElement ); const geometry = new THREE.BoxGeometry( 1, 1, 1 ); const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); const cube = new THREE.Mesh( geometry, material ); scene.add( cube ); camera.position.z = 5; function animate() { cube.rotation.x += 0.01; cube.rotation.y += 0.01; renderer.render( scene, camera ); }
+// Import libraries
+import * as THREE from 'three';
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// Create a renderer
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+
+// Set the output color space
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+// Set the size of the renderer
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+// Add the renderer to the DOM
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+// Create a cube
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// Create a material
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// Create a mesh
+const cube = new THREE.Mesh( geometry, material );
+// Add the mesh to the scene
+scene.add( cube );
+
+// Add a plane
+const planeGeometry = new THREE.PlaneGeometry( 10, 10 );
+const planeMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+
+// Rotate plane
+plane.rotation.x = - Math.PI / 2;
+plane.position.y = - 0.5;
+
+// Add the plane to the scene
+scene.add( plane );
+
+// Set the position of the camera
+camera.position.z = 5;
+
+
+// Render the scene
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render( scene, camera );
+}
